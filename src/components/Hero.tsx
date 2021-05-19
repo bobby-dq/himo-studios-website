46561ecded25cf06e7f1db746f742f6a5228307e
bobby-dq <josh.quilacio@gmail.com>
2021-05-19 11:10:35 -0600

    added animations to the hero section.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,43 +6,46 @@ import { Link } from 'react-router-dom'
 
 // Styles, animations, and illustrations
 import { heroWaveAnimation } from '../styles/animations'
-import { ReactComponent as PhoneDesktopSVG} from '../svgs/hero-desktop-phone.svg'
+import PhoneDesktopSVG from '../svgs/hero-desktop-phone.svg';
+import { heroTitleAnimation, sectionLoadAnimation, glowingButtonAnimation, textFadeInAnimation } from '../styles/animations';
 
 export const Hero: FunctionComponent = () => {
 
     return (
-        <SHero>
+        <SHero variants={sectionLoadAnimation} initial="hidden" animate="show">
             <div className="hero-title">
                 <div className="tiny-text">
-                    <p>UNPARALLELED CRAFTMANSHIP</p>
+                    <motion.p variants={textFadeInAnimation} >UNPARALLELED CRAFTMANSHIP</motion.p>
                 </div>
                 <motion.div className="main-title">
                     <div className="main-title-text">
-                        <h2>Expand your brand's</h2>
+                        <motion.h2 variants={heroTitleAnimation}>Expand your brand's</motion.h2>
                     </div>
                     <div className="main-title-text colored-text">
-                        <h2 className="colored-text">online presence</h2>
+                        <motion.h2 variants={heroTitleAnimation} className="colored-text">online presence</motion.h2>
                     </div>
                     <div className="main-title-text">
-                        <h2>with expert advice.</h2>
+                        <motion.h2 variants={heroTitleAnimation}>with expert advice.</motion.h2>
                     </div>
                     <div className="main-title-text">
                         <p>From <span className="colored-text">website design</span>, <span className="colored-text">development</span>, <br/><span className="colored-text">to SEO</span>, we got you covered.</p>
                     </div>
                 </motion.div>
-                <Link to="/contact">Get Started</Link>
+                <motion.div variants={glowingButtonAnimation} style={{display: 'inline-block'}}>
+                    <Link to="/contact">Get Started</Link>
+                </motion.div>
             </div>
             <div className="hero-img">
-                <PhoneDesktopSVG />
+                <img src={PhoneDesktopSVG} alt="hero-svg" />
             </div>
             <SWave viewBox="0 0 1440 363" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <motion.path variants={heroWaveAnimation} initial="hidden" animate="show" d="M1440 27.4774C1352.73 19.8184 1122.41 49.0556 899.331 227.276C620.48 450.052 354.282 355.647 170.328 185.318C23.165 49.0556 -4.21721 8.32998 0.487081 5" stroke="#D6AB78" strokeOpacity="0.2" strokeWidth="10"/>
+                <motion.path variants={heroWaveAnimation} d="M1440 27.4774C1352.73 19.8184 1122.41 49.0556 899.331 227.276C620.48 450.052 354.282 355.647 170.328 185.318C23.165 49.0556 -4.21721 8.32998 0.487081 5" stroke="#D6AB78" strokeOpacity="0.2" strokeWidth="10"/>
             </SWave>
         </SHero>
     );
 }
 
-const SHero = styled.div`
+const SHero = styled(motion.div)`
     min-height: 50vh;
     padding: 1rem 0rem;
     display: flex;
@@ -71,12 +74,16 @@ const SHero = styled.div`
 
     }
 
+    .hero-img, .hero-title{
+        z-index: 2;
+    }
+
     @media(max-width: 900px) {
         align-items: center;
         justify-content: space-between;
 
         .hero-img {
-            svg {
+            img {
                 margin: 2rem;
                 width: 100%;
             }
@@ -89,14 +96,13 @@ const SHero = styled.div`
         .hero-img {
             position: absolute;
             opacity: 0.04;
+            z-index: 0;
         }
     }
 
    
 
-    .hero-img, .hero-title{
-        z-index: 2;
-    }
+    
 
     .hero-title {
         a {
@@ -106,7 +112,7 @@ const SHero = styled.div`
             padding: 0.5rem 3rem;
             font-family: 'Playfair Display', serif;
             background: #73FFEE;
-            box-shadow: 5px 5px 100px 10px #43A498, inset 1px 1px 10px 0.5px rgba(0, 0, 0, 0.5);
+            /* box-shadow: 5px 5px 100px 10px #43A498, inset 1px 1px 10px 0.5px rgba(0, 0, 0, 0.5); */
         }
     }
 
@@ -115,7 +121,12 @@ const SHero = styled.div`
         font-family: 'Montserrat', sans-serif;
         font-weight: bold;
         letter-spacing: 0.50rem;
-        color: #817676
+        color: #817676;
+        overflow: hidden;
+    }
+
+    .main-title-text {
+        overflow: hidden;
     }
 `;
 
@@ -124,6 +135,7 @@ const SWave = styled.svg`
     left: 0;
     
 `;
+
 
 
 
--- src/styles/animations.ts
+++ src/styles/animations.ts
@@ -24,3 +24,62 @@ export const heroWaveAnimation = {
         }
     }
 }
+
+export const heroTitleAnimation = {
+    hidden: {
+        y: '4rem'
+    },
+    show: {
+        y: 0,
+        transition: {
+            duration: 0.75,
+            ease: "easeOut",
+        }
+    }
+}
+
+export const glowingButtonAnimation = {
+    hidden: {
+        boxShadow: '0px 0px 0px 0px #43A4980',
+        opacity: 0
+    },
+    show: {
+        opacity: 1,
+        boxShadow: '5px 5px 100px 10px #43A498',
+        transition: {
+            duration: 0.33,
+            ease: "easeOut"
+        }
+    }
+
+}
+
+export const sectionLoadAnimation = {
+    hidden: {
+        opacity: 0,
+        scale: 3
+    },
+    show: {
+        scale: 1,
+        opacity: 1,
+        transition: {
+            duration: 1,
+            ease: "easeOut",
+            when: 'beforeChildren',
+            staggerChildren: 0.25
+        }
+    }
+}
+
+export const textFadeInAnimation = {
+    hidden: {
+        x: -1000,
+    },
+    show: {
+        x: 0,
+        transition: {
+            duration: 0.33,
+            ease: "easeOut"
+        }
+    }
+}
