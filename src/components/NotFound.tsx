45ce01aef9290f4e3e4ba2573a7e588278cfdc42
bobby-dq <josh.quilacio@gmail.com>
2021-05-23 23:43:31 -0600

    added a back to home link in the error404 page.

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,5 +1,6 @@
 // Pacakges
 import { FunctionComponent } from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 // Styles and illustrations\\
@@ -10,7 +11,8 @@ export const NotFound: FunctionComponent = () => {
     return  (
         <SNotFound>
             <div className="text-wrapper">
-                <h1>Oops, see like we can't find what youre looking for..</h1>
+                <h1>Oops, seems like we can't find what youre looking for..</h1>
+                <Link to="/">Back to Home</Link>
             </div>
             <div className="image-wrapper">
                 <img src={notFound} alt="" />
@@ -38,6 +40,24 @@ const SNotFound = styled.div`
         font-size: 2.4rem;
         font-family: 'Monteserrat', sans-serif;
         text-align: center;
+
+        h1 {
+            margin-bottom: 2rem;
+        }
+
+        a {
+            font-size: 2.4rem;
+            font-weight: bold;
+            color: #1b1b1b;
+            padding: 0.5rem 3rem;
+            font-family: 'Playfair Display', serif;
+            background: #73FFEE;
+            transition: all ease 0.33s;
+            &:hover {
+                box-shadow: 5px 5px 50px 10px #43A498;
+                color: #3c3b3b;
+            }
+        }
     }
 
     .image-wrapper {
